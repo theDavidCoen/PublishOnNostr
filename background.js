cde09480e05ed21b240590d154fa02f4ec271777
David Coen <info@davidcoen.it>
2023-01-19 15:43:42 +0100

    Update background.js

--- background.js
+++ background.js
@@ -5,17 +5,21 @@ chrome.contextMenus.create({
     // Step 1: Copy the URL of the img in the current web page
     navigator.clipboard.writeText(info.srcUrl);
 
-    // Step 2: Go to https://snort.social/
-    chrome.tabs.create({ url: "https://snort.social/" });
+    // Step 2: Get the URL from chrome storage
+    chrome.storage.local.get(['nostr-webclient-url'], function(result) {
+        var url = result['nostr-webclient-url'] || 'https://snort.social/';
+        // Step 3: Go to the URL retrieved from chrome storage or the default one
+        chrome.tabs.create({ url: url });
 
-    // Step 3: Paste the URL from point 1 into <textarea class="textarea" placeholder="Say something!"></textarea>
-    chrome.tabs.executeScript({
-      code: "document.querySelector('textarea.textarea').value = navigator.clipboard.readText();"
-    });
+        // Step 4: Paste the URL from point 1 into <textarea class="textarea" placeholder="Say something!"></textarea>
+        chrome.tabs.executeScript({
+            code: "document.querySelector('textarea.textarea').value = navigator.clipboard.readText();"
+        });
 
-    // Step 4: Tap Send
-    chrome.tabs.executeScript({
-      code: "document.querySelector('input[type=submit]').click();"
+        // Step 5: Tap Send
+        chrome.tabs.executeScript({
+            code: "document.querySelector('input[type=submit]').click();"
+        });
     });
   }
 });
